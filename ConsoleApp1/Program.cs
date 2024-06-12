89eb9491d3d6134b792e3f04e83e1cef05f962a9
Leonardo <leonarboatarde@gmail.com>
2024-06-12 20:39:53 -0300

    Leitura do arquivo

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string arquivo = @"C:\Users\lleon\OneDrive - sga.pucminas.br\2° semestre\Algoritmo e Estrutura de Dados\Trabalho\teste.txt"; // Caminho do arquivo
+
+// Usando um bloco "using" para garantir o fechamento automático do arquivo
+using (StreamReader reader = new StreamReader(arquivo))
+{
+    string linha;
+
+    while ((linha = reader.ReadLine()) != null)
+    {
+        Console.WriteLine(linha);
+    }
+
+    Console.ReadKey();
+}
