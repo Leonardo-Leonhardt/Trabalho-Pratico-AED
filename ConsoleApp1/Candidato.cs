9dbea51efc6038d441b91bf58e83edf9f060a617
Leonardo <leonarboatarde@gmail.com>
2024-06-13 14:45:41 -0300

    .
    .

---
+++ ConsoleApp1/Candidato.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class Candidato
+{
+    public Candidato()
+    {
+
+  private string Nome;
+    private float NotaRedacao;
+    private float NotaMatematica;
+    private float NotaLinguagem;
+
+    public Candidato(string nome, float notaRedacao, float notaMatematica)
+    {
+        Console.WriteLine(nome);
+    }
+}
+
